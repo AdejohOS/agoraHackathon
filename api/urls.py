7eddc723f6b17535a0733022356c0c082432355c
Adejoh OS <AdejohOS>
2022-11-03 20:02:56 +0100

    Latest

---
+++ api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . views import AdvocateList, AdvocateDetail
+from . import views
+
+urlpatterns = [
+   
+    path('', views.getRoutes),
+
+    path('advocates/', AdvocateList.as_view(), name='advocate_list'),
+    path('advocates/<int:pk>/', AdvocateDetail.as_view(), name='advocate_detail'),
+
+]
